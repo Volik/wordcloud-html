77902f1774e9e15ee8c674d04f8eac88cd01a536
IcedNecro <romanstatkevich@gmail.com>
2015-06-04 15:13:37 +0300

    verbatim translation design

--- design/js/pricing-tool.js
+++ design/js/pricing-tool.js
@@ -1,22 +1,9 @@
 $(document).ready(function() {
-    var lastOpened='.pt-language-content';
-    var lastOpenedSelector='.pt-language-selector';
+    var lastOpened='';
 
-    $('.pt-verbatims-translation-selector').click(
-        function() {
-            $('.pt-results-wrapper').hide();
+    var lastOpenedSelector='';
 
-            $(lastOpened).hide();
-
-            $(this).toggleClass('pt-verbatims-translation-selector-active');
-            
-            $(lastOpenedSelector).removeClass(lastOpenedSelector+'-active');
-            $('.pt-verbatims-translation-content').fadeIn();
-            $('.buttons-wrapper').fadeIn();
-            lastOpened = '.pt-verbatims-translation-content';
-            lastOpenedSelector = '.pt-verbatims-translation-selector';
-        }
-    )
+    var previousSelectedButton=null;
 
     $('.pt-options-list a').hover(
         function() {
@@ -39,15 +26,15 @@ $(document).ready(function() {
             $('.pt-results-wrapper').hide();
             // hide all other containers
             $(lastOpened).hide();
+            // remove all other active classes
+            $(lastOpenedSelector).removeClass(lastOpenedSelector.substring(1)+'-active');
             // set the active class
             $(this).toggleClass('pt-language-selector-active');
-            // remove all other active classes
-            $(lastOpenedSelector).removeClass(lastOpenedSelector+'-active');
             // show current container and form controls
             $('.pt-language-content').fadeIn();
             $('.buttons-wrapper').fadeIn();
             lastOpened = '.pt-language-content';
-            lastOpenedSelector = '.pt-questions-selector';
+            lastOpenedSelector = '.pt-language-selector';
         }
     );
 
@@ -55,12 +42,12 @@ $(document).ready(function() {
         function() {
             // hide results
             $('.pt-results-wrapper').hide();
+            // remove all other active classes
+            $(lastOpenedSelector).removeClass(lastOpenedSelector.substring(1)+'-active');
             // hide all other containers
             $(lastOpened).hide();
             // set the active class
             $(this).toggleClass('pt-sample-size-selector-active');
-            // remove all other active classes
-            $(lastOpenedSelector).removeClass(lastOpenedSelector+'-active');
             // show current container and form controls
             $('.pt-sample-size-content').fadeIn();
             $('.buttons-wrapper').fadeIn();
@@ -75,10 +62,10 @@ $(document).ready(function() {
             $('.pt-results-wrapper').hide();
             // hide all other containers
             $(lastOpened).hide();
+            // remove all other active classes
+            $(lastOpenedSelector).removeClass(lastOpenedSelector.substring(1)+'-active');
             // set the active class
             $(this).toggleClass('pt-questions-selector-active');
-            // remove all other active classes
-            $(lastOpenedSelector).removeClass(lastOpenedSelector+'-active');
             // show current container and form controls
             $('.pt-questions-content').fadeIn();
             $('.buttons-wrapper').fadeIn();
@@ -87,4 +74,34 @@ $(document).ready(function() {
         }
     );
 
+    $('.pt-verbatims-translation-selector').click(
+        function() {
+            // hide results
+            $('.pt-results-wrapper').hide();
+            // hide all other containers
+            $(lastOpened).hide();
+            // remove all other active classes
+            $(lastOpenedSelector).removeClass(lastOpenedSelector.substring(1)+'-active');
+            // set the active class
+            $(this).toggleClass('pt-verbatims-translation-selector-active');
+            
+            $('.pt-verbatims-translation-content').fadeIn();
+            $('.buttons-wrapper').fadeIn();
+            lastOpened = '.pt-verbatims-translation-content';
+            lastOpenedSelector = '.pt-verbatims-translation-selector';
+        }
+    )
+    
+    /*$('.button-verbatim-translation').click(
+        function() {
+            if(previousSelectedButton !=  null) {
+                $(previousSelectedButton).hide(0);
+                $(previousSelectedButton).removeClass("button-verbatim-translaton-selected");
+            } 
+            alert(previousSelectedButton);
+            $(this).toggleClass('button-verbatim-translaton-selected');
+            $(this).fadeIn();
+            previousSelectedButton = this;
+        }         
+    )*/
 });
